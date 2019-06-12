a074b9d23a4dd1296e694dcb544d722bd9938548
Carrosen <carla1.rosen@gmail.com>
2019-06-12 14:05:57 +0200

    Installs npm package Google Map React, creates google API key, creates Map component. Map is now visible :D

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import SimpleMap from './Components/Map.jsx'
 
 function App() {
   return (
-   <h1>
-     Being Stockholm
-   </h1>
+    <>
+    <h1>
+      Being Stockholm
+    </h1>
+    <SimpleMap />
+   </>
   );
 }
 
---
+++ src/Components/Map.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+ 
+  render() {
+
+    const handleApiLoaded = (map, maps) => {
+      // use map and maps objects
+    };
+
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCJ0bE4IpvmyulOxE84uiEYEilIVBwPu6c' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
