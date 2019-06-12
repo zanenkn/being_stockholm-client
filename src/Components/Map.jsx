fd77c518a9d253ccce30bb28126123aa8948a952
stefan_karlberg <stefan.karlberg@smackit.se>
2019-06-12 16:15:04 +0200

    Adds plus sign on top of map

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import MapStyle from '../Modules/MapStyle'
+import { Icon } from 'semantic-ui-react';
+
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
@@ -18,6 +20,7 @@ class Map extends Component {
     return (
     
       <div style={{ height: '100vh', width: '100%' }}>
+      <Icon style={{ position: 'absolute', zIndex: '4000', padding: '1rem'}}name='plus' size='huge' color='orange' />
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCJ0bE4IpvmyulOxE84uiEYEilIVBwPu6c' }}
           defaultCenter={this.props.center}
@@ -29,7 +32,7 @@ class Map extends Component {
             lng={18.068562}
             text="Hello Sthml!"
           /> */}
-
+ 
         </GoogleMapReact>
       </div>
     );
