5aa0e877f81bec4a0078e8dec73d5e8de6e529aa
stefan_karlberg <stefan.karlberg@smackit.se>
2019-06-13 20:28:44 +0200

    Adds a footer and adjust height of the map.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-// import { Icon } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 class Footer extends Component {
   render() {
     return(
-      <p>Test</p>
+      <Grid id='footer' celled='internally'>
+        <Grid.Row>
+        <Grid.Column width={3}>
+          <p>test</p>
+        </Grid.Column>
+        <Grid.Column width={10}>
+        <p>test</p>
+        </Grid.Column>
+        <Grid.Column width={3}>
+        <p>test</p>
+        </Grid.Column>
+      </Grid.Row>
+      </Grid>
     )
   }
 }
