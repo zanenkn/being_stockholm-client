d238f1b2c32b9beefcc6173d2b2f38abe1bc0549
raptorf1 <raptor_f1@hotmail.com>
2019-06-14 11:17:59 +0200

    Tries to make site height to be 100% on desktop screens only. Not successfull.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import { Grid, Icon, Header} from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 class Footer extends Component {
   render() {
-    return(
+    return (
       <Grid id='footer' celled='internally'>
         <Grid.Row>
-        <Grid.Column textAlign='center' width={3}>
-          <Icon name='bars'/>
-        </Grid.Column>
-        <Grid.Column textAlign='center' width={10}>
-        <Header color="orange" id="footer-logo" as='h3'>Being Stockholm</Header>
-        </Grid.Column>
-        <Grid.Column textAlign='center' width={3}>
-          <Icon circular name='user' />
-        </Grid.Column>
-      </Grid.Row>
+          <Grid.Column textAlign='center' width={3}>
+            <Icon name='bars' />
+          </Grid.Column>
+          <Grid.Column textAlign='center' width={10}>
+            <Header color="orange" id="footer-logo" as='h3'>Being Stockholm</Header>
+          </Grid.Column>
+          <Grid.Column textAlign='center' width={3}>
+            <Icon circular name='user' />
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     )
   }
