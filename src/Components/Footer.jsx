4910dc4658a56bdc4acd5ebeabc95617254c4b39
raptorf1 <raptor_f1@hotmail.com>
2019-06-14 11:31:47 +0200

    Changes the footer icons to orange

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,13 +7,17 @@ class Footer extends Component {
       <Grid id='footer' celled='internally'>
         <Grid.Row>
           <Grid.Column textAlign='center' width={3}>
-            <Icon name='bars' />
+            <Icon
+              name='bars'
+              color='orange' />
           </Grid.Column>
           <Grid.Column textAlign='center' width={10}>
             <Header color="orange" id="footer-logo" as='h3'>Being Stockholm</Header>
           </Grid.Column>
           <Grid.Column textAlign='center' width={3}>
-            <Icon circular name='user' />
+            <Icon circular
+              name='user'
+              color='orange' />
           </Grid.Column>
         </Grid.Row>
       </Grid>
