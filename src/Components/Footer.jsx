8e0a9062e1ec092112abe77809faddd91afdd61c
zanenkn <zane.neikena@gmail.com>
2019-06-15 00:05:53 +0200

    experimental with zondicons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,14 +5,10 @@ class Footer extends Component {
   render() {
     return (
 
-        <Grid id='footer' verticalAlign='middle'>
-          <Grid.Row>
-            <Grid.Column textAlign='center' width={3}>
-              <Icon
-                id='footer-menu-icon'
-                name='bars'
-                color='orange'
-                onClick={this.props.handleAnimationChange('overlay')} />
+        <Grid id='footer' verticalAlign='middle' centered columns={3}>
+     
+            <Grid.Column textAlign='center'  verticalAlign='middle' width={3}>
+            <svg height = '5vw' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F2711C" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg> 
             </Grid.Column>
             <Grid.Column textAlign='center' width={10}>
               <Header
@@ -23,12 +19,9 @@ class Footer extends Component {
             </Header>
             </Grid.Column>
             <Grid.Column textAlign='center' width={3}>
-              <Icon circular
-                id='footer-user-icon'
-                name='user'
-                color='orange' />
+            <svg height = '5vw' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F2711C" d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
             </Grid.Column>
-          </Grid.Row>
+
         </Grid>
     )
   }
