e9af375bb19995175e2ebc7b53f004b06b254d01
felix_bonnier <felix_bonnier@yahoo.com>
2019-06-14 16:54:42 +0200

    changes Grid in footer to not have celled, and aligns all icons center.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
-import { Grid, Icon, Header } from 'semantic-ui-react';
+import { Grid, Icon, Header, Container } from 'semantic-ui-react';
 
 class Footer extends Component {
   render() {
     return (
-      <Grid id='footer' celled='internally'>
-        <Grid.Row>
-          <Grid.Column textAlign='center' width={3}>
-            <Icon
-              id='footer-menu-icon'
-              name='bars'
-              color='orange'
-              onClick={this.props.handleAnimationChange('overlay')} />
-          </Grid.Column>
-          <Grid.Column textAlign='center' width={10}>
-            <Header
-              color='orange'
-              id='footer-logo'
-              as='h3'>
-              Being Stockholm
+
+        <Grid id='footer' verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column textAlign='center' width={3}>
+              <Icon
+                id='footer-menu-icon'
+                name='bars'
+                color='orange'
+                onClick={this.props.handleAnimationChange('overlay')} />
+            </Grid.Column>
+            <Grid.Column textAlign='center' width={10}>
+              <Header
+                color='orange'
+                id='footer-logo'
+                as='h3'>
+                Being Stockholm
             </Header>
-          </Grid.Column>
-          <Grid.Column textAlign='center' width={3}>
-            <Icon circular
-              id='footer-user-icon'
-              name='user'
-              color='orange' />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+            </Grid.Column>
+            <Grid.Column textAlign='center' width={3}>
+              <Icon circular
+                id='footer-user-icon'
+                name='user'
+                color='orange' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
     )
   }
 }
