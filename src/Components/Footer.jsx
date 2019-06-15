ccc0958e4ddf9c852129e10b6efadc6c2d7d3145
zanenkn <zane.neikena@gmail.com>
2019-06-15 11:35:06 +0200

    Adds back the onClick function on the menu icon.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Icon, Header, Container } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 
 class Footer extends Component {
   render() {
@@ -8,7 +8,7 @@ class Footer extends Component {
         <Grid id='footer' verticalAlign='middle' centered columns={3}>
      
             <Grid.Column textAlign='center'  verticalAlign='middle' width={3}>
-            <svg height = '5vw' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F2711C" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg> 
+              <svg height = '5vw' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" onClick={this.props.handleAnimationChange('overlay')}><path fill="#F2711C" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg> 
             </Grid.Column>
             <Grid.Column textAlign='center' width={10}>
               <Header
@@ -19,7 +19,7 @@ class Footer extends Component {
             </Header>
             </Grid.Column>
             <Grid.Column textAlign='center' width={3}>
-            <svg height = '5vw' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F2711C" d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
+              <svg height = '5vw' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F2711C" d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
             </Grid.Column>
 
         </Grid>
