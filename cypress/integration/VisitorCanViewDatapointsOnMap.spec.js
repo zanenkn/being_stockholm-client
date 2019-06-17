7b8f5838d9ae563d894761c5eccaa6f0e4df4f1a
felix_bonnier <felix_bonnier@yahoo.com>
2019-06-17 15:40:21 +0200

    adds feature test for finding datapoints on map. Adds fixture file for mock posts from index serializer. Test fails

---
+++ cypress/integration/VisitorCanViewDatapointsOnMap.spec.js
@@ -0,0 +1,26 @@
+describe('Visitor can view on the map', () => {
+
+  it('datapoints of posts with correct coordinates', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3002/api/v1/posts',
+      response: 'fixture:list_of_posts.json',
+      status: 200
+    })
+    cy.visit('http://localhost:3000')
+
+    let posts = [
+      "#post_1","#post_2", "#post_3","#post_4", "#post_5"
+    ]
+
+    posts.forEach(post => {
+      cy.get(post).should('be_visible')
+      })
+    })
+
+    //Test that we find datapoints on the map (cy.get the correct Div Id?)
+    //Test that datapoints are in the right position on the map?
+    //Test that datapoints have the right color, based on category?
+
+})
