35ddcedaf966a730fbea221d0db132c6079f4cee
Carrosen <carla1.rosen@gmail.com>
2019-06-19 10:06:41 +0200

    Installs redux, adds basic folder and file structure, adds initialState for sidebarVisibility

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import * as serviceWorker from './serviceWorker';
 import './semantic/dist/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
+import { Provider } from 'react-redux'
+import configureStore from './state/store/configureStore';
 
 axios.defaults.baseURL = 'http://localhost:3002'
 
+const store = configureStore();
+window.store = store
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
 
 serviceWorker.unregister();
---
+++ src/state/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import initialState from '../store/initialState'
+
+const rootReducer = (state = initialState) => {
+  return state
+}
+export default rootReducer
