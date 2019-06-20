88a75215c91bb5a04841f0a73209c80a4d918aed
raptorf1 <raptor_f1@hotmail.com>
2019-06-20 22:35:58 +0200

    Imports Geocode in EntryPopup component to extract and display address from specific entry

--- src/Components/EntryPopup.jsx
+++ src/Components/EntryPopup.jsx
@@ -1,31 +1,51 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
+import Geocode from 'react-geocode'
 
 class EntryPopup extends Component {
+
   state = {
     caption: '',
     category: '',
     created_at: '',
-    image: ''
+    image: '',
+    latitude: '',
+    longitude: '',
+    address: ''
   }
 
   componentDidMount() {
-    axios.get('/api/v1/posts/'+`${this.props.id}`).then(response => {
-      this.setState({ 
+    axios.get('/api/v1/posts/' + `${this.props.id}`).then(response => {
+      this.setState({
         caption: response.data.caption,
         category: response.data.category,
         created_at: response.data.created_at,
-        image: response.data.image
-      });
-    });
+        image: response.data.image,
+        latitude: response.data.latitude,
+        longitude: response.data.longitude
+      })
+    })
   }
 
   render() {
-    
+
+    let dataAndTime
+
+    Geocode.setApiKey(process.env.REACT_APP_API_KEY_GOOGLE_MAPS)
+    Geocode.fromLatLng(parseFloat(this.state.latitude), parseFloat(this.state.longitude)).then(
+      response => {
+        const address = response.results[0].formatted_address
+        this.setState({ address: address })
+      },
+      error => {
+        console.error(error);
+      }
+    )
+
     return (
-        <>
-        <p>yo</p>
-        </>
+      <>
+        {this.state.address}
+      </>
     )
   }
 }
