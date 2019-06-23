3d4f936feee9cde2c6af0a2961b530c980343820
stefan_karlberg <stefan.karlberg@smackit.se>
2019-06-23 12:08:13 +0200

    Adds integration test for user profile page.

---
+++ cypress/integration/UserCanViewProfilePage.js
@@ -0,0 +1,36 @@
+describe('User can', () => {
+
+  beforeEach(function () {
+    cy.server()
+  })
+
+  it('view user page', () => {
+
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3002/api/v1/auth/sign_in',
+      status: 200,
+      response: 'fixture:successful_login.json',
+      headers: {
+        "uid": "carla@mail.com"
+      }
+    })
+    cy.visit('http://localhost:3000')
+    cy.get('#profile-icon').click()
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('carla@mail.com')
+      cy.get('#password').type('password')
+    })
+    cy.get('#login_form_button').click()
+    cy.wait(3000)
+    cy.get('#profile-icon').click()
+    cy.contains('Hello Carla')
+  })
+
+  it('not view profile page if not logged in', () => {
+    
+    cy.visit('http://localhost:3000')
+    cy.get('#profile-icon').click()
+    cy.contains('Hello Carla').should('not.exist')
+  })
+})
