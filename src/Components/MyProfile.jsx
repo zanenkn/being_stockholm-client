5dd5d37a4b70dfc7891d77a0cd11af5ad7a3d282
stefan_karlberg <stefan.karlberg@smackit.se>
2019-06-23 12:57:54 +0200

    Connects MyProfile component to Redux. Manage Sidebar visibilty with Redux. Adds header.

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Header, Container, Sidebar } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
-function MyProfile() {
-  return (
-    <p>Yo!</p>
+class MyProfile extends Component {
+  render() {
+
+  return(
+    <Sidebar.Pushable as={Container} id="views-main-container-sidebar">
+    <div onClick={this.props.sidebarVisible ? () => { this.props.dispatch({ type: 'CHANGE_VISIBILITY' }) } : () => { }}>
+      <Container className="views-main-container">
+        <Header className="views-main-header" as='h1'>
+          My profile 
+        </Header>
+        <br></br>
+      </Container>
+    </div>
+    </Sidebar.Pushable>
   )
 } 
+}
 
-export default MyProfile
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.reduxTokenAuth.currentUser,
+    sidebarVisible: state.animation.sidebarVisible
+  }
+}
+
+export default connect(mapStateToProps)(MyProfile);
