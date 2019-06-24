bdb9d8f31ecc02cc280e8c3cbb44148fd37ceb77
zanenkn <zane.neikena@gmail.com>
2019-06-24 10:12:23 +0200

    Adds the link to the actual survey, minor styling fixes.

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -4,11 +4,6 @@ import { connect } from 'react-redux'
 
 class MyProfile extends Component {
   render() {
-    let nameMatch = (this.props.currentUser.attributes.uid).match(/^([^@]*)@/);
-    let name = nameMatch ? nameMatch[1] : null;
-    let greetingName = name.charAt(0).toUpperCase() + name.slice(1)
-    let greeting = `Hello ${greetingName}!`
-
     return (
       <Sidebar.Pushable as={Container} id="views-main-container-sidebar">
         <div fluid onClick={this.props.sidebarVisible ? () => { this.props.dispatch({ type: 'CHANGE_VISIBILITY' }) } : () => { }}>
@@ -17,25 +12,22 @@ class MyProfile extends Component {
               My profile
             </Header>
             <br></br>
-            <Grid centered>
-            <p>
-              {greeting}
-            </p>
-            <p className="views-text-container" >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <Divider hidden />
-            <p className='text'>
-              Please help us make Being Stockholm better and answer six questions about yourself.
-            </p>         
-            <Button
-              className="submit-button"
-              href="https://github.com"
-              target="_blank"
-            >
-              Take me to the survey
-            </Button>
-          </Grid>
+            <Container className="align-center">
+              <p >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <Divider hidden />
+              <p>
+                Please help us make Being Stockholm better and answer six questions about yourself.
+              </p>         
+              <Button
+                className='submit-button'
+                href='https://urbanbeings.us18.list-manage.com/subscribe?u=511ba4646c76ccebddfc09524&id=4b6589bfcd'
+                target='_blank'
+              >
+                Take me to the survey
+              </Button>
+            </Container>
           </Container>
         </div>
       </Sidebar.Pushable>
