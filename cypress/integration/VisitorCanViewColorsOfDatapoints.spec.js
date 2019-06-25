c7fba23eaa01c2c71f812991f878f96dba5384f5
Carrosen <carla1.rosen@gmail.com>
2019-06-25 13:39:46 +0200

    Adds test for datapoints showing the correct category colors, WIP

--- cypress/integration/VisitorCanViewColorsOfDatapoints.spec.js
+++ cypress/integration/VisitorCanViewColorsOfDatapoints.spec.js
@@ -14,7 +14,24 @@ describe('Visitor can view on the map', () => {
   it('datapoints of posts only if they are published', () => {
 
     let postsPublished = [
-      "#post_1", "#post_3",  "#post_6",  "#post_7",  "#post_8",  "#post_9",  "#post_10", "#post_11"
+      "#post_6", "#post_7", "#post_8", "#post_9", "#post_10", "#post_11"
     ]
+
+    postsPublished.forEach(post => {
+      cy.get(post).should('be.visible')
+    })
+  })
+
+  it('datapoints in different category colors', () => {
+
+    // work
+    cy.get('#post_6').should('have.class', 'teal')
+    cy.get('#post_7').should('have.class', 'teal')
+    cy.get('#post_8').should('have.class', 'teal')
+
+    // play
+    cy.get('#post_9').should('have.class', 'teal')
+    cy.get('#post_10').should('have.class', 'teal')
+    cy.get('#post_11').should('have.class', 'teal')
   })
 })
