584f62068891c7b02451612e683753ddf7a77062
felix_bonnier <felix_bonnier@yahoo.com>
2019-06-27 15:22:17 +0200

    adds more content to FAQ component, as per requirements

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Header, Sidebar, Container  } from 'semantic-ui-react'
+import { Header, Sidebar, Container, List } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 
 
@@ -13,6 +13,79 @@ class Faq extends Component {
                 FAQs
               </Header>
               <br></br>
+
+              <Header className="views-third-header" as='h4'>
+                What is the best way to upload a post?
+              </Header>
+              <Container className="views-text-container">
+                <p>
+                  On your phone’s web browser!
+                </p>
+                <p>
+                  You can upload from your photo album (recommended) or even take a photo directly from your camera. You can also use your tablet or computer. If your settings allow it, you can even upload from Google Drive, Dropbox, iCloud etc
+                </p>
+              </Container>
+              <br></br>
+
+              <Header className="views-third-header" as='h4'>
+                Why can’t I map my post?
+              </Header>
+              <Container className="views-text-container">
+                <p>
+                  Please check it contains all of the following:
+                </p>
+                <List bulleted>
+                  <List.Item>An image</List.Item>
+                  <List.Item>A short caption of less than 140 characters</List.Item>
+                  <List.Item>A location of where the photo was taken If the photo contains geolocation data, we can map it automatically. Otherwise you can enter and search for the address manually.</List.Item>
+                  <List.Item>Tap on the toggle to describe if the post is work or play</List.Item>
+                  <List.Item>Click ‘Map it!’</List.Item>
+                </List>
+              </Container>
+              <br></br>
+
+              <Header className="views-third-header" as='h4'>
+                Why is my post declined?
+              </Header>
+              <Container className="views-text-container">
+                <p>
+                  Mistakes happen! Maybe your little one got hold of your phone and uploaded a blurry photo and gibberish text.
+                </p>
+                <p>
+                  Or we feel the post has an inappropriate photo or caption that may cause offence - but hopefully not criminal!
+                </p>
+                <p>
+                  For the beta version, we can only accept photos from within Stockholm. Global domination will follow, don’t worry! Watch this space! If you haven’t already, subscribe to our <a href='https://urbanbeings.us18.list-manage.com/subscribe?u=511ba4646c76ccebddfc09524&id=4b6589bfcd' target="_blank">mailing list</a>&nbsp;here.
+                </p>
+              </Container>
+              <br></br>
+
+              <Header className="views-third-header" as='h4'>
+                How can I see my photos?
+              </Header>
+              <Container className="views-text-container">
+                <p>
+                You can see a list of all your posts in <a><b>My profile</b></a> - those pending to be reviewed, those declined and those published.
+                </p>
+              </Container>
+              <br></br>
+
+              <Header className="views-third-header" as='h4'>
+                How can I share my post with my friends?
+              </Header>
+              <Container className="views-text-container">
+                <p>
+                  Unfortunately this beta version isn’t able to share individual posts or datapoints. It is on the to-do list for the next iteration though!
+                </p>
+                <p>
+                  However you can still share the project website with your friends, so they can see the map and even better - share their photos too! Simply direct them to <a href='http://www.beingstockholm.se/' target="_blank">www.beingstockholm.se.</a>&nbsp;Thanks in advance!
+                </p>
+              </Container>
+              <br></br>
+
+
+
+
             </Container>
           </div>
         </Sidebar.Pushable>
