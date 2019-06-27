f79023537ffe08d6890e9c51dff1334ab6bb427e
Carrosen <carla1.rosen@gmail.com>
2019-06-27 14:57:49 +0200

    Adds circle icons to legend with all the different category colors

--- src/Components/Legend.jsx
+++ src/Components/Legend.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Segment, Sidebar, Grid, Menu, Sticky, Icon } from 'semantic-ui-react'
+import { Segment, Sidebar, Grid, Menu, Sticky, Icon, Header } from 'semantic-ui-react'
 
 class Legend extends Component {
 
@@ -9,23 +9,91 @@ class Legend extends Component {
     return (
       <>
         <Sidebar
-        className='legend-content'
-        id='legend'
-        as={Segment}
-        animation='overlay'
-        direction='bottom'
-        visible={this.props.visible}
-        onClick={() => this.props.dispatch({ type: 'CHANGE_LEGEND_VISIBILITY' })}
+          className='legend-content'
+          id='legend'
+          as={Segment}
+          animation='overlay'
+          direction='bottom'
+          visible={this.props.visible}
+          onClick={() => this.props.dispatch({ type: 'CHANGE_LEGEND_VISIBILITY' })}
         >
           <Grid>
-            <Grid.Column textAlign='center' className="legend-padding" > 
-            <Icon name='window minimize outline' color='grey' className="legend-icon" size='large' />
-              <p>Hej Carla!</p>
+            <Grid.Column textAlign='center' className="legend-padding" >
+              <Icon
+                name='window minimize outline' 
+                color='grey' 
+                className="legend-icon" 
+                size='large' 
+              />
+
+              <br></br>
+
             </Grid.Column>
+          </Grid>
+
+          <Grid>
+            <Grid.Column width={8}>
+              <Header>
+                <Icon
+                  id='circle-my-work'
+                  name='circle'
+                  size='large'>
+                </Icon>
+                My work
+              </Header>
+
+              <Header>
+                <Icon
+                  id='circle-settled-work'
+                  name='circle'
+                  size='large'>
+                </Icon>
+                Settled Work
+              </Header>
+
+              <Header>
+                <Icon
+                  id='circle-newbie-work'
+                  name='circle'
+                  size='large'>
+                </Icon>
+                Nembie Work
+              </Header>
+            </Grid.Column>
+
+            <Grid.Column width={8}>
+              <Header>
+                <Icon
+                  id='circle-my-play'
+                  name='circle'
+                  size='large'>
+                </Icon>
+                My play
+              </Header>
+
+              <Header>
+                <Icon
+                  id='circle-settled-play'
+                  name='circle'
+                  size='large'>
+                </Icon>
+                Settled Play
+              </Header>
+
+              <Header>
+                <Icon
+                  id='circle-newbie-play'
+                  name='circle'
+                  size='large'>
+                </Icon>
+                Newbie Play
+              </Header>
+            </Grid.Column>
+
           </Grid>
         </Sidebar>
       </>
-     
+
     )
   }
 }
