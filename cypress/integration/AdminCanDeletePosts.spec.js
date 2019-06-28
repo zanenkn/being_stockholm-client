e10351a8936d3b66c229e86dbd727636d3ed9667
Carrosen <carla1.rosen@gmail.com>
2019-06-28 10:06:17 +0200

    Creates test for Admin can delete an entry

---
+++ cypress/integration/AdminCanDeletePosts.spec.js
@@ -0,0 +1,45 @@
+describe('Admin can delete an entry and', () => {
+
+  beforeEach(function () {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3002/api/v1/posts',
+      response: 'fixture:list_of_posts.json',
+      status: 200
+    })
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3002/api/v1/auth/sign_in',
+      status: 200,
+      response: 'fixture:successful_login_admin.json',
+      headers: {
+        "uid": "yatwan@mail.com"
+      }
+    })
+    cy.route({
+      method: 'DELETE',
+      url: 'http://localhost:3002/api/v1/posts/2',
+      status: 200,
+      response: 'fixture:successful_delete_from_admin.json',
+      headers: {
+        "uid": "yatwan@mail.com"
+      }
+    })
+    cy.visit('http://localhost:3000')
+    cy.get('#profile-icon').click()
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('yatwan@mail.com')
+      cy.get('#password').type('password')
+    })
+    cy.get('#login_form_button').click()
+    cy.wait(3000)
+  })
+
+  it('by clicking on the delete button on an Entry', () => {
+      cy.get('#3').click()
+      cy.get('#delete-button').click()
+      // cy.contains('You have successfully deleted your post')
+      cy.get('#3').should('not.be.visible')
+  })
+})
